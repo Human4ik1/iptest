6450ed65de6276bdbbb40bfb5269881495ec29ad
Илья Кряжик <Human4ikman4ik@gmail.com>
2025-07-19 17:47:02 +0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Обновление системы
+apt update
+apt upgrade -y
+
+# Установка sudo
+apt install -y sudo
+
+# Установка speedtest
+sudo apt-get install -y curl
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+sudo apt-get install -y speedtest
+
+# Установка caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+chmod o+r /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install -y caddy
+
+# Автоматический запуск speedtest с подтверждением
+echo -e "YES\nYES" | speedtest
+
+echo "Настройка завершена!"
